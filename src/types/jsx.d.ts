0c96fe310371a6838b2cf0c579c7a3df3ffb02c8
kyprexs <xwest4487@gmail.com>
2025-05-13 21:52:40 -0700

    test: force git to recognize change

--- src/types/jsx.d.ts
+++
@@ -1,9 +0,0 @@
-     import { Object3D } from 'three';
-
-     declare global {
-       namespace JSX {
-         interface IntrinsicElements {
-           primitive: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { object: Object3D };
-         }
-       }
-     }
