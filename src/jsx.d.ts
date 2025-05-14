9c704a0d7d605c7ed4c6181338878ccca8e2f326
kyprexs <xwest4487@gmail.com>
2025-05-13 21:27:34 -0700

    fix: move JSX primitive type declaration to src root and update tsconfig

---
+++ src/jsx.d.ts
@@ -0,0 +1,9 @@
+     import { Object3D } from 'three';
+
+     declare global {
+       namespace JSX {
+         interface IntrinsicElements {
+           primitive: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { object: Object3D };
+         }
+       }
+     }
